8e815ba20ff9b8ed40388706e0956e7b5c889f93
roylin7 <roylin7@hotmail.com>
2017-11-18 17:52:05 -0600

    foodllog.swift added

---
+++ Project-Alpha/Foodlog.swift
@@ -0,0 +1,30 @@
+//
+//  Foodlog.swift
+//  Project-Alpha
+//
+//  Created by Roy Lin on 11/18/17.
+//  Copyright © 2017 Roy Lin. All rights reserved.
+//
+
+import Foundation
+
+class Foodlog{
+    var year: String
+    var month: String
+    var day : String
+    var foodname : String
+    var carb : String
+    var fat : String
+    var protein : String
+    
+    
+    init(year: String, month: String, day: String,foodname:String, carb: String, fat: String , protein: String) {
+        self.year = year
+        self.month = month
+        self.day = day
+        self.foodname = foodname
+        self.carb = carb
+        self.fat = fat
+        self.fat = fat
+    
+}
