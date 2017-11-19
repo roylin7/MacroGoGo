380f877c8bc749257eebae540af5422be31f7f20
Sheryar Ali <Sheryar@wireless-10-145-57-154.public.utexas.edu>
2017-11-19 17:04:42 -0600

    settings screen

--- Project-Alpha/Setting.swift
+++ Project-Alpha/Setting.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class Setting: UIViewController {
-
+    var alertController:UIAlertController? = nil
     @IBOutlet weak var lbltargetfat: UITextField!
     @IBOutlet weak var lbltargetpotein: UITextField!
     @IBOutlet weak var targetcarbohydrate: UITextField!
@@ -19,6 +19,37 @@ class Setting: UIViewController {
     @IBOutlet weak var lblheight: UITextField!
     @IBOutlet weak var lblweight: UITextField!
     
+    @IBAction func BMIindex(_ sender: Any) {
+        if lblweight.text == "" {
+            self.alertController = UIAlertController(title: "Error", message: "Please fill in weight.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            
+            let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                print("Ok Button Pressed 1");
+            }
+            self.alertController!.addAction(OKAction)
+            
+            self.present(self.alertController!, animated: true, completion:nil)
+        }
+        else if lblheight.text == "" {
+                self.alertController = UIAlertController(title: "Error", message: "Please fill in height.", preferredStyle: UIAlertControllerStyle.alert)
+                
+                
+                let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                    print("Ok Button Pressed 1");
+                }
+                self.alertController!.addAction(OKAction)
+                
+                self.present(self.alertController!, animated: true, completion:nil)
+            }
+        else {
+            var weight = Int(lblweight.text!)! * 703
+            var height = Int(lblheight.text!)! ^ 2
+            var bmi = weight / height
+            lblbmi.text = String(bmi)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
