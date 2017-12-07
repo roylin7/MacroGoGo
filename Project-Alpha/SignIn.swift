5e93b127ae83f944ff85e5c37b559322cf42a351
Sarah McCarthy <worldf4sarahmccarthy@gmail.com>
2017-12-06 20:28:28 -0600

    interface colors changed for cleaner, more uniform appearance; keyboard dismiss implemented partially; logo and icon update; working on launch screen

--- Project-Alpha/SignIn.swift
+++ Project-Alpha/SignIn.swift
@@ -10,12 +10,13 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-class SignIn: UIViewController {
+class SignIn: UIViewController, UITextFieldDelegate {
 
     // UI text fields for username and password associated with account
     // to be filled out upon login prompt
     @IBOutlet weak var lblusername: UITextField!
     @IBOutlet weak var lblpw: UITextField!
+    @IBOutlet weak var signinbtn: UIButton!
     
     @IBOutlet weak var lblmsg: UILabel!
     override func viewDidLoad() {
@@ -24,7 +25,8 @@ class SignIn: UIViewController {
         // page header
         DataStore.shared.loadPeople()
         self.title = "Sign-In"
-      
+        lblusername.delegate = self
+        lblpw.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -34,15 +36,12 @@ class SignIn: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-   
-    
     // select "Login" button to verify account info in datastore
     @IBAction func btnlogin(_ sender: Any) {
         
         let email = String.makeFirebaseString(lblusername.text!)
         if email() == " " || lblpw.text! == " " {
             lblmsg.text = "One or more credentials is missing, please try again!"
-            
         }
         
         if DataStore.shared.usernameExist(username: email()){
@@ -51,7 +50,6 @@ class SignIn: UIViewController {
            if person.pw == lblpw.text!{
                 lblmsg.text = "Login successful"
                 self.performSegue(withIdentifier: "hehe", sender: self)
-            
                 
                 Auth.auth().signIn(withEmail: lblusername.text!, password: lblpw.text!) { (user, error) in
                     if error != nil{
@@ -63,13 +61,23 @@ class SignIn: UIViewController {
                 }
            }
            else if person.pw != lblpw.text!{
-                lblmsg.text = "One or more credentials is invalid, please try again!!"
+                lblmsg.text = "One or more credentials is invalid, please try again!"
            }
         }
         else {
-            lblmsg.text = "One or more credentials is invalid, please try again!!!"
+            lblmsg.text = "One or more credentials is invalid, please try again!"
         }
     }
+    
+    // text field dismiss method
+    // called when user touches return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 'First Responder' is the same as 'input focus'.
+        // We are removing input focus from the text field.
+        textField.resignFirstResponder()
+        return true
+    }
+    
     // Called when user touches main view (outside the UITextField).
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
