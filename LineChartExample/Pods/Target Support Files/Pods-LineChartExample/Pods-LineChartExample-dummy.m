420f377e20d580beca5466ba3292607f2319d37d
Sarah McCarthy <worldf4sarahmccarthy@gmail.com>
2017-12-09 19:58:52 -0600

    cleaning up repo

--- LineChartExample/Pods/Target Support Files/Pods-LineChartExample/Pods-LineChartExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LineChartExample : NSObject
-@end
-@implementation PodsDummy_Pods_LineChartExample
-@end
--- LineChartExample/Pods/Target Support Files/Pods-LineChartExampleUITests/Pods-LineChartExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LineChartExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LineChartExampleUITests
-@end
